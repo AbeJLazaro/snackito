98c0ba79d99b5b284a7e21a7fae6330026e15393
abraham_lazaro <abraham.lazaro@rappi.com>
2023-01-09 19:39:28 -0600

    codigo del sensor

--- proyectofinal/fisico.py
+++ proyectofinal/fisico.py
@@ -1,5 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+TRIG = 4
+ECHO = 18
+total_distance = 50 #CM
+ 
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
 def leer_sensor():
-	return 70
+	GPIO.output(TRIG, True)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, False)
+	
+	while GPIO.input(ECHO) == False:
+		start = time.time()
+	
+	while GPIO.input(ECHO) == True:
+		end = time.time()
+	
+	sig_time = end-start
+	
+	#CM:
+	distance = sig_time / 0.000058
+	
+	GPIO.cleanup()
+
+	porcentaje = (distance / 100) * total_distance
+
+	return (round(porcentaje/10)*10)%100
 
 def mover_motor(n):
 	print(f"El motor se movio {n} veces")
